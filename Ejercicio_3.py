5c4501f79eebd3b2f08fe8441f64af50f6b1c86a
luciacantos <luciacantos22@gmail.com>
2023-01-19 11:05:36 +0100

    Cálculo área del parche

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -6,3 +6,6 @@ def max_cuerda(diametro, porcentaje): #calcular la máxima longitud de la cuerda
 
     #Calcular el radio del parche circular de hierba
     radio = diametro / 2
+
+    #Calcular el área del parche
+    area = math.pi * (radio ** 2)
