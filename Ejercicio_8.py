4959e5b0e96055811ef68d845bb7641d2e7aa430
luciacantos <luciacantos22@gmail.com>
2023-01-19 11:47:12 +0100

    comentario

--- Ejercicio_8.py
+++ Ejercicio_8.py
@@ -4,4 +4,5 @@ import re
 
 def encontrar_monstruo(texto):
     monstruo = re.search(r"(tree fiddy|3.50|three fifty)", texto, re.IGNORECASE) #busca la palabra tree fiddy, 3.50 o three fifty
-
+    # re.IGNORECASE es una bandera que permite que la expresión regular coincida con mayúsculas y minúsculas.
+    
