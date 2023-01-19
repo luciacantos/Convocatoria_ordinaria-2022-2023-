6da01cfdcda4e562dc777c347acec3e1523c78f4
luciacantos <luciacantos22@gmail.com>
2023-01-19 11:15:32 +0100

    valor para cada número

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -1,3 +1,5 @@
 # Ejercicio 3
 
-def numero_letras(n): 
+def numero_letras(n):
+    #escribir la palabra de cada número en inglés para contar en nº de letras en una lista
+    valores = ["zero","one","two","three","four","five","six","seven","eight","nine"]
