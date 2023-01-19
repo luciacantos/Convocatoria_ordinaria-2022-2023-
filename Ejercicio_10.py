84db16c7873719657329e3af9c203b9eaee2aaf3
luciacantos <luciacantos22@gmail.com>
2023-01-19 11:57:05 +0100

    Ejemplos

--- Ejercicio_10.py
+++ Ejercicio_10.py
@@ -5,3 +5,8 @@ def xbonacci(firma, n): #devuelve una lista con los n primeros números de la se
     for i in range(n-len(firma)):
         secuencia.append(sum(secuencia[-len(firma):]))
     return secuencia
+
+if __name__ == "__main__":
+    print("xbonacci {1,1,1,1} 10 =",xbonacci([1,1,1,1], 10))
+    print("xbonacci {0,0,0,0,1} 10 =",xbonacci([0,0,0,0,1], 10))
+    print("xbonacci {1,0,0,0,0,0,1} 10 =",xbonacci([1,0,0,0,0,0,1], 10))
