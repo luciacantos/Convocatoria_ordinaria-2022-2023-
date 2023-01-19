54e1a356cb074e55ed7fcb61c74059999e4eb40a
luciacantos <luciacantos22@gmail.com>
2023-01-19 11:19:26 +0100

    bucle para contar las letras

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -3,3 +3,10 @@
 def numero_letras(n):
     #escribir la palabra de cada número en inglés para contar en nº de letras en una lista
     valores = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+    #lista vacía para añadir el resultado
+    resultado = []
+
+    while n != 4: #4 es el único número que su palaabra tiene el mismo número de letras que su valor
+        palabra = ""
+        cont = n
+        
