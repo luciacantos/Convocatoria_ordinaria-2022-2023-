fa1a1d90f25868636be5e2ed596fca95a6676c08
luciacantos <luciacantos22@gmail.com>
2023-01-19 11:51:30 +0100

    cálculo edad

--- Ejercicio_9.py
+++ Ejercicio_9.py
@@ -1,3 +1,7 @@
 #Ejercicio 9
 
 import datetime
+
+def numero_lunes(cumpleaños, fecha_actual):
+    # calcular edad
+    edad = fecha_actual.year - cumpleaños.year - ((fecha_actual.month, fecha_actual.day) < (cumpleaños.month, cumpleaños.day))
