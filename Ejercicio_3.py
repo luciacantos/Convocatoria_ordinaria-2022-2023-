225102e5b7505825d79f7192a50f3a3e772e2d2d
luciacantos <luciacantos22@gmail.com>
2023-01-19 11:28:37 +0100

    corrección

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -1,12 +1,11 @@
-# Ejercicio 3
+#Ejercicio 3
 
 def numero_letras(n):
     #escribir la palabra de cada número en inglés para contar en nº de letras en una lista
-    valores = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+    valores = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     #lista vacía para añadir el resultado
     resultado = []
-
-    while n != 4: #4 es el único número que su palaabra tiene el mismo número de letras que su valor
+    while n != 4: #4 es el único número que su palabra tiene el mismo número de letras que su valor
         palabra = ""
         cont = n
         while cont > 0:
@@ -14,8 +13,8 @@ def numero_letras(n):
             cont = cont // 10
         resultado.append(palabra)
         n = len(palabra)
-        resultado.append("four")
-        return resultado
+    resultado.append("four")
+    return resultado
 
 if __name__ == "__main__":
     print(numero_letras(60))
