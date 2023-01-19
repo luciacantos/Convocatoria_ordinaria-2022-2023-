46f325b90dd2ccd6e720078fa555abd75c267403
luciacantos <luciacantos22@gmail.com>
2023-01-19 11:35:09 +0100

    dibujo triángulo

--- Ejercicio_5.py
+++ Ejercicio_5.py
@@ -1,5 +1,13 @@
 #Ejercicio 5
 
 def hollow_triangle(altura): #devuelve un triángulo hueco de la altura indicada
+    triangulo = []
 
-    
+    for i in range(1, altura+1): #dibujar el triángulo
+        line = '_'*(altura-i) + '#'
+        if i > 1:
+            line += '_'*(2*i-3) + '#'
+        line += '_'*(altura-i)
+        triangulo.append(line)
+    triangulo.append('#'*(2*altura-1))
+    return triangulo
