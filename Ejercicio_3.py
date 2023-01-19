e9415cf182f288208be7c0ea84e871282caa3494
luciacantos <luciacantos22@gmail.com>
2023-01-19 11:04:24 +0100

    cálculo radio

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -3,4 +3,6 @@
 import math
 
 def max_cuerda(diametro, porcentaje): #calcular la máxima longitud de la cuerda
-    
+
+    #Calcular el radio del parche circular de hierba
+    radio = diametro / 2
