bfdc0b912380ca2b67e8cfaeea3e90df440272c0
luciacantos <luciacantos22@gmail.com>
2023-01-19 12:21:08 +0100

    corrección

--- Ejercicio_4.py
+++ Ejercicio_4.py
@@ -3,18 +3,32 @@
 import re
 from typing import Dict, List, Tuple
 
-def do_math(numeros: str) -> int: #encontar las letras y los números
-    def extract_letter(numero: str) -> Tuple[str, int]:
-        letra = re.search(r"[a-zA-Z]", numero).group()
-        numero = int(re.sub(r"[a-zA-Z]", "", numero))
-        return letra, numero
+def do_math(numbers: str) -> int:
+    def extract_letter(number: str) -> Tuple[str, int]:
+        letter = re.search(r"[a-zA-Z]", number).group()
+        number = int(re.sub(r"[a-zA-Z]", "", number))
+        return letter, number
 
-    numeros_dict = {}
-    for number in numeros.split():
-        letra, numero = extract_letter(numero)
-        if letra in numeros_dict:
-            numeros_dict[letra].append(numero)
+    numbers_dict = {}
+    for number in numbers.split():
+        letter, number = extract_letter(number)
+        if letter in numbers_dict:
+            numbers_dict[letter].append(number)
         else:
-            numeros_dict[letra] = [numero]
+            numbers_dict[letter] = [number]
 
-        
+    sorted_numbers = [num for letter in sorted(numbers_dict) for num in numbers_dict[letter]]
+
+    result = sorted_numbers[0]
+    operations = "+-*/"
+    operation_index = 0
+    for i in range(1, len(sorted_numbers)):
+        operation = operations[operation_index % 4]
+        result = eval(f"{result} {operation} {sorted_numbers[i]}")
+        operation_index += 1
+    return round(result)
+
+print("24z6 1x23 y369 89a 900b ->",do_math("24z6 1x23 y369 89a 900b"))
+print("24z6 1z23 y369 89z 900b ->",do_math("24z6 1z23 y369 89z 900b"))
+print("10a 90x 14b 78u 45a 7b 34y ->",do_math("10a 90x 14b 78u 45a 7b 34y"))
+
