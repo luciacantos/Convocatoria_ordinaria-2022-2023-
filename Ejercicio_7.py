c5a9577c9b9627ef864ba0c3f2f556f93bbb3e3d
luciacantos <luciacantos22@gmail.com>
2023-01-19 11:40:01 +0100

    coordenadas en notación algebráica

--- Ejercicio_7.py
+++ Ejercicio_7.py
@@ -3,3 +3,8 @@
 from collections import deque
 #Un objeto deque es un contenedor de datos del módulo collections similar a una lista o una cola que
 #permite añadir o suprimir elementos por sus dos extremos.
+
+def caballo(inicial, final):
+    # convertir las posiciones de notación algebraica a coordenadas (fila, columna)
+    inicial_pos = (8 - int(inicial[1]), ord(inicial[0]) - ord('a'))
+    final_pos = (8 - int(final[1]), ord(final[0]) - ord('a'))
