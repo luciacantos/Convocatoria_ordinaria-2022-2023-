d153fcecac9007f0f431f24540f2bcca3f94c45c
luciacantos <luciacantos22@gmail.com>
2023-01-19 11:38:25 +0100

    Importacón de 'deque'

---
+++ Ejercicio_7.py
@@ -0,0 +1,5 @@
+#Ejercicio 7
+
+from collections import deque
+#Un objeto deque es un contenedor de datos del módulo collections similar a una lista o una cola que
+#permite añadir o suprimir elementos por sus dos extremos.
